1288cbb035f911bb66f716d60397301a5057a595
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-11 18:05:38 +0200

    linked the login and the admin page

--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -2,10 +2,12 @@ const patient = document.getElementById("patient");
 const employee = document.getElementById("employee");
 const service = document.getElementById("service");
 const stats = document.getElementById("stats");
+const logout = document.getElementById("logout");
 
 patient.addEventListener("click", () => {
   window.location.href = "./assignpatient.html";
 });
+
 employee.addEventListener("click", () => {
   window.location.href = "./assignemployee.html";
 });
