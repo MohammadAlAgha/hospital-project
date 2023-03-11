27078c5056a58a4904dc588881189b4120a01e35
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-11 12:18:06 +0200

    Styled the sign up page

--- backend/login.php
+++ backend/login.php
@@ -5,7 +5,7 @@ include('connection.php');
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$query=$mysqli->prepare('select id,name,email from users where email=?');
+$query=$mysqli->prepare('select id,name,email,user_type from users where email=?');
 $query->bind_param('s',$email);
 $query->execute();
 $result=$query->get_result();
