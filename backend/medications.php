064b1581e52c6aeebf367aad2e618d870b76e7a5
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-12 09:06:03 +0200

    Linked the profile page with the patient main page

--- backend/medications.php
+++ backend/medications.php
@@ -5,14 +5,14 @@ $medication=$_GET['medication'];
 $query=$mysqli->prepare('select * from medications where id=?');
 $query->bind_param('i',$medication);
 $query->execute();
+$result=$query->get_result();
 
-$query->get_result();
 
 while($object=$result->fetch_assoc()){
     $data=$object;
 }
 
-$response['medications']=$data;
+$response['medication']=$data;
 
 echo json_encode($response);
 
