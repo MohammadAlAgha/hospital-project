b824e87acff4d79ef8ed96318149153600f37fd4
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-12 20:23:31 +0200

    Added the assign patients to the data base

--- frontend/scripts/assignpatient.js
+++ frontend/scripts/assignpatient.js
@@ -1,6 +1,8 @@
 const back = document.getElementById("back");
 const hospitalNames = document.getElementById("hospitalNames");
 const userNames = document.getElementById("userNames");
+const submit = document.getElementById("submit");
+const state = document.getElementById("state");
 const baseUrl = "http://localhost/hospital-project/backend";
 
 back.addEventListener("click", () => {
@@ -13,8 +15,9 @@ axios({
   console.log(res.data.hospitals);
   const hospitalsArray = res.data.hospitals;
   hospitalsArray.forEach((hospital) => {
-    hospitalNames.innerHTML += ` <option>${hospital.name}</option>`;
+    hospitalNames.innerHTML += ` <option id='user'>${hospital.name}</option>`;
   });
+  const user = document.getElementById("user");
 });
 axios({
   url: `${baseUrl}/getuser.php`,
@@ -22,6 +25,15 @@ axios({
   console.log(res.data.users);
   const usersArray = res.data.users;
   usersArray.forEach((user) => {
-    userNames.innerHTML += ` <option>${user.name}/ID:${user.id}</option>`;
+    userNames.innerHTML += ` <option id='hospital'>${user.name}/ID:${user.id}</option>`;
   });
+  const hospital = document.getElementById("hospital");
+});
+
+submit.addEventListener("click", () => {
+  axios
+    .get(`${baseUrl}/assignuser.php?${userNames}&${hospitalNames}`)
+    .then((res) => {
+      state.innerHTML = "Patient Assigned";
+    });
 });
